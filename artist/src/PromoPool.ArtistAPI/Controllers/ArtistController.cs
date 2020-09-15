12fde4b3732635d3ef9b29a424961ebad4e9658b
Steve Owens <stevejowens@icloud.com>
2020-09-15 23:15:27 +0100

    updated source

--- artist/src/PromoPool.ArtistAPI/Controllers/ArtistController.cs
+++ artist/src/PromoPool.ArtistAPI/Controllers/ArtistController.cs
@@ -107,51 +107,7 @@ namespace PromoPool.ArtistAPI.Controllers
                     return BadRequest(ModelState);
                 }
             }
-
             return BadRequest();
-
-
-        }
-
-        [HttpGet("{id}/logo")]
-        [ProducesResponseType(StatusCodes.Status202Accepted)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        [Produces("application/json")]
-        [Authorize]
-        public IActionResult UploadLogoAsync(string id)
-        {
-            logger.LogInformation($"UploadLogo id: {id} - Resource Requested.");
-
-            try
-            {
-                var file = Request.Form.Files[0];
-                var folderName = Path.Combine("Resources", "Images");
-                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
-                if (file.Length > 0)
-                {
-                    var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
-                    var fullPath = Path.Combine(pathToSave, fileName);
-                    var dbPath = Path.Combine(folderName, fileName);
-                    using (var stream = new FileStream(fullPath, FileMode.Create))
-                    {
-                        file.CopyTo(stream);
-                    }
-                    return Ok(new { dbPath });
-                }
-                else
-                {
-                    return BadRequest();
-                }
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, $"Internal server error: {ex}");
-            }
-
-
-        }
-
-        
+        }        
     }
 }
